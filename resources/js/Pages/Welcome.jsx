e2db687ac43e9980d7a125bbf514b1ea5e856e83
new name <sanyamgoswami21@gmail.com>
2023-12-11 14:53:25 +0530

    testing

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -8,7 +8,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                 <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
                     {auth.user ? (
                         <Link
-                            href={route('dashboard')}
+                            href={route('admin.dashboard')}
                             className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                         >
                             Dashboard
@@ -21,7 +21,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                             >
                                 Log in
                             </Link>
-{/* 
+{/*
                             <Link
                                 href={route('register')}
                                 className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
